831e04cfd2a3a9e6ee6c25f068ab8c8178bbf4da
Tainá Saboia <tasaboia@users.noreply.github.com>
2023-01-29 19:12:22 -0300

    mobile fixed

--- src/components/activities/Activities.tsx
+++ src/components/activities/Activities.tsx
@@ -12,17 +12,17 @@ export default function Activities() {
   return (
     <Box sx={{width: "100%", height: 700,display: "flex", flexDirection: "column", justifyContent: { xs: "flex-start", sm: "center"}, paddingLeft: { xs: "2rem", sm:"6.5rem"},  paddingRight: { xs: "2rem", sm:"6.5rem"}, marginTop:{ xs: "4rem", sm:"0"}}}>
          <Box >
-            <Box sx={{ width: { xs: "100%", sm: "50%"}, }}>
+            <Box sx={{ width: { xs: "100%", sm: "70%"}, }}>
                 <Typography  fontFamily="Fira Sans" variant="h5" gutterBottom>{ActivitiesTitle}</Typography>
                 <Typography color="#495456" lineHeight={"30px"} fontFamily="Open Sans" variant="h6" gutterBottom>{ActivitiesCards.subtitle1}</Typography>
                 <Typography color="#495456" lineHeight={"30px"} fontFamily="Open Sans" variant="h6" gutterBottom>{ActivitiesCards.subtitle2}</Typography>
 
             </Box>
          </Box>
-         <Box sx={{ overflow: {xs :"scroll", sm:"hidden"}, width: "100%", flexDirection: "row", display: "flex", alignItems:"start", justifyContent: { xs : "flex-start", sm:"space-between"}}}>
-            <Box component="div" sx={{ width: 1000, display: "flex", flexDirection: "row"}}>
+         <Box sx={{overflow: {xs :"scroll", sm:"hidden"}, width: "100%", height: 300, flexDirection: "row", display: "flex", alignItems:"start", }}>
+            <Box component="div" sx={{ width: {xs:1000, sm:"100%"}, display: "flex", flexDirection: "row", paddingBottom:'1rem', justifyContent: { xs : "flex-start", sm:"space-between"}}}>
                 {ActivitiesCards.cards.map((item: IActivitiesCard, index) => (
-                    <Card sx={{ width: 233, height: 233, marginRight: "1rem", marginTop: "2rem" }}>
+                    <Card sx={{bgcolor:"#EBEBEB", width: 233, height: 233, marginRight: "1rem", marginTop: "2rem" }}>
                         <CardActionArea>
                         <CardContent>
                             <img  src={item.icon} alt={`icone ${item.title}`} />
