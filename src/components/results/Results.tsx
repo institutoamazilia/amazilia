b5e95d5ac365cc472780052652b09641a75f5670
Tainá Saboia <tasaboia@users.noreply.github.com>
2023-01-29 22:53:52 -0300

    Results added

--- src/components/results/Results.tsx
+++ src/components/results/Results.tsx
@@ -1,39 +1,7 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
-const data = {
-  title: "Quais resultados já alcançamos?",
-  subt: "Dados acumulados de 2020 a maio de 2022.",
-  cards: [
-    {
-      title: "Impacto direto em",
-      values: [
-        {
-          value: "170",
-          title: "gestores sociais",
-        },
-        {
-          value: "70",
-          title: "OSCs participantes",
-        }
-      ]
-    },
-    {
-      title: "Sedes das OSCs",
-      values: [
-        {
-          value: "18",
-          title: "Estados brasileiros",
-        },
-        {
-          value: "113",
-          title: "voluntários engajados",
-        }
-      ]
-    }
-  ],
-}
+import { data } from './data';
 
 export default function Results() {
   return (
---
+++ src/components/results/data.tsx
@@ -0,0 +1,33 @@
+
+export const data = {
+    title: "Quais resultados já alcançamos?",
+    subt: "Dados acumulados de 2020 a maio de 2022.",
+    cards: [
+      {
+        title: "Impacto direto em",
+        values: [
+          {
+            value: "170",
+            title: "gestores sociais",
+          },
+          {
+            value: "70",
+            title: "OSCs participantes",
+          }
+        ]
+      },
+      {
+        title: "Sedes das OSCs",
+        values: [
+          {
+            value: "18",
+            title: "Estados brasileiros",
+          },
+          {
+            value: "113",
+            title: "voluntários engajados",
+          }
+        ]
+      }
+    ],
+  }
