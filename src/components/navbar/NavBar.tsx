6126fb59b85223e88fbb4bb36c2ae85f8cb2f194
MelissaWengBr <meweng@gmail.com>
2023-02-06 15:31:55 -0300

    novos titulos da barra de navegacao

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -16,9 +16,9 @@ import { v4 as uuidv4 } from "uuid";
 
 const pages = [
   "Home",
-  "Atividades que fazemos",
+  "O que fazemos",
   "Próximos Editais",
-  "Testemunhos",
+  "Depoimentos",
   "Apoiadores",
 ];
 
