04e463862798eaa44e686a0a03fdae25d8531a27
Tainá Saboia <tasaboia@users.noreply.github.com>
2023-02-11 21:47:37 -0300

    onu versao mobile

--- src/components/contribution/Contribution.tsx
+++ src/components/contribution/Contribution.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { data } from "./data";
 import { v4 as uuidv4 } from "uuid";
 export default function Contribution() {
@@ -8,11 +8,11 @@ export default function Contribution() {
       id="Contribution"
       sx={{
         display: "flex",
-        flexDirection: "row",
+        flexDirection: { xs: "column-reverse", sm: "row" },
         bgcolor: "#ECEDEE",
-        paddingLeft: "6rem",
-        paddingTop: "4rem",
-        paddingBottom: "4rem",
+        paddingLeft: { xs: "1rem", sm: "6rem" },
+        paddingTop: "2rem",
+        paddingBottom: "2rem",
         gap: "4rem",
       }}
     >
@@ -20,16 +20,18 @@ export default function Contribution() {
         <Typography fontSize="28px" color="#522D6D" fontFamily="Fira Sans">
           {data.title}
         </Typography>
-        <Typography maxWidth="60%" my="1.4rem">
+        <Typography sx={{ maxWidth: { xs: "100%", sm: "60%" } }} my="1.4rem">
           {data.subt}
         </Typography>
-        {data.images.map((item, index) => (
-          <img key={uuidv4()} style={{ marginRight: "1.5rem" }} src={item} />
-        ))}
+        <Box>
+          {data.images.map((item, index) => (
+            <img key={uuidv4()} style={{ marginRight: ".4rem" }} src={item} />
+          ))}
+        </Box>
       </Box>
-      <Box>
+      <Container>
         <img src={data.onuImage} />
-      </Box>
+      </Container>
     </Box>
   );
 }
